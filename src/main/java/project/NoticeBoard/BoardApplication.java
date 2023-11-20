321326595a83c7c3fd439a126e83265eff11f5f9
hyeon-gyu <hyungue729@naver.com>
2023-11-20 17:21:28 +0900

    first commit

---
+++ src/main/java/project/NoticeBoard/BoardApplication.java
@@ -0,0 +1,13 @@
+package project.NoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
